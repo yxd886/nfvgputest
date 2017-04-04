da124e701ec653b7f4e6b9ec4124ad7a52a6fdab
xiaodongyi <409070169@qq.com>
2017-04-04 18:02:32 +0800

    change makefire

--- runtime/d_nf/d_base/Pkt.h
+++ runtime/d_nf/d_base/Pkt.h
@@ -3,16 +3,19 @@
 
 #ifndef PKT_H
 #define PKT_H
-#define PKT_SIZE 319
-#define FS_STATE_MAX_SIZE 199
+#define PKT_SIZE 400
+#define FS_STATE_MAX_SIZE 160
 
-#pragma pack(32)
+#pragma pack(4)
 struct  Pkt{
 	bool empty;
+	char pad1;
+	char pad2;
+	char pad3;
 	char pkt[PKT_SIZE];
 };
 
-#pragma pack(32)
+#pragma pack(8)
 struct  Fs{
 	uint64_t actor_id_64;
 	char fs[8][FS_STATE_MAX_SIZE];
