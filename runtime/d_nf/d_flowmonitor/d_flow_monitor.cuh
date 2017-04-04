64e5d0156d2a4dfe9c9cae28c197ed746693e523
xiaodongyi <409070169@qq.com>
2017-04-04 21:05:11 +0800

    change makefire

--- runtime/d_nf/d_flowmonitor/d_flow_monitor.cuh
+++ runtime/d_nf/d_flowmonitor/d_flow_monitor.cuh
@@ -56,7 +56,7 @@ __device__ void process(char* raw_packet,d_flow_monitor_fs* fs){
       struct d_head_info* hd=&t;
       Format(raw_packet,hd);
       //fs->CreatedTime=time(0);
-      fs->SrcIp = Ntohl(hd->m_pIphdr->saddr);
+      fs->SrcIp = (hd->m_pIphdr->saddr);
       fs->DstIp = Ntohl(hd->m_pIphdr->daddr);
       fs->protocol   = hd->m_pIphdr->protocol;
       if(hd->m_pTcphdr==NULL){
