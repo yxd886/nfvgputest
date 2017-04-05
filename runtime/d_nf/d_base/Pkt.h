14b927237bd5b245c941a9ca6dda003c34deb47d
xiaodongyi <409070169@qq.com>
2017-04-05 12:10:00 +0800

    format out of gpu

--- runtime/d_nf/d_base/Pkt.h
+++ runtime/d_nf/d_base/Pkt.h
@@ -5,7 +5,38 @@
 #define PKT_H
 #define PKT_SIZE 400
 #define FS_STATE_MAX_SIZE 160
+#include <rte_config.h>
+#include <rte_memory.h>
+#include <rte_memzone.h>
+#include <rte_launch.h>
+#include <rte_eal.h>
+#include <rte_per_lcore.h>
+#include <rte_lcore.h>
+#include <rte_debug.h>
+#include <rte_common.h>
+#include <rte_log.h>
+#include <rte_malloc.h>
+#include <rte_memory.h>
+#include <rte_memcpy.h>
+#include <rte_memzone.h>
+#include <rte_eal.h>
+#include <rte_per_lcore.h>
+#include <rte_launch.h>
+#include <rte_atomic.h>
+#include <rte_cycles.h>
+#include <rte_prefetch.h>
+#include <rte_lcore.h>
+#include <rte_per_lcore.h>
+#include <rte_branch_prediction.h>
+#include <rte_interrupts.h>
+#include <rte_pci.h>
+#include <rte_random.h>
+#include <rte_debug.h>
 #include <rte_ether.h>
+#include <rte_ethdev.h>
+#include <rte_ring.h>
+#include <rte_mempool.h>
+#include <rte_mbuf.h>
 
 struct d_headinfo{
   struct ether_hdr m_pEthhdr;
