5d9df0c16da2a146bc1f6e64af1270567bd33359
xiaodongyi <409070169@qq.com>
2017-04-05 09:50:07 +0800

    change makefire

--- runtime/d_nf/d_flowmonitor/d_flow_monitor.cuh
+++ runtime/d_nf/d_flowmonitor/d_flow_monitor.cuh
@@ -89,7 +89,7 @@ __device__ void process(char* raw_packet,d_flow_monitor_fs* fs){
     __device__ void Format(char* packet,struct d_head_info* hd){
     hd->m_pEthhdr = (struct ether_hdr*)packet;
     hd->m_pIphdr = (struct iphdr*)(packet + sizeof(struct ether_hdr));
-    //hd->m_pTcphdr = (struct tcphdr*)(packet + sizeof(struct ether_hdr)+(hd->m_pIphdr->ihl)*4);
+    hd->m_pTcphdr = (struct tcphdr*)(packet + sizeof(struct ether_hdr)+(hd->m_pIphdr->ihl)*4);
     hd->m_pTcphdr =NULL;
     hd->protocol =  hd->m_pIphdr->protocol;
     return;
