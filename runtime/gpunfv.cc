e7088a293f8d4998ccae2ae33fd11fb98308cf4a
xiaodongyi <409070169@qq.com>
2017-04-06 16:14:40 +0800

    format out of gpu

--- runtime/gpunfv.cc
+++ runtime/gpunfv.cc
@@ -131,7 +131,7 @@ void test(){
     struct timeval whole_begin;
     gettimeofday(&whole_begin,0);
 
-    for(int j=0;j<100;j++){
+    for(int j=0;j<300;j++){
        fread(head,34,1,f);
  	  m_pEthhdr=(struct ether_hdr *)head;
  	  m_pIphdr=(struct iphdr *)(head+sizeof(struct ether_hdr));
